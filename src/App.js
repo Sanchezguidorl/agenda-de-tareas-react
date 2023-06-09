ca21836ae1c48baa6f194ffde6d1881b8a07b113
chenaoh <sanchezguidorl@gmail.com>
2023-06-08 22:54:13 -0300

    Creación de componente de tipo Tarea y agregación de estilos css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Tarea from './componentes/Tarea';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="free-contenedor-logo">
+        <img
+          src={require("./imagenes/free-logo.png")}
+          className="free-logo"
+          alt="logo de freecodecamp"
+        />
+      </div>
+      <div className="main">
+        <h1>Mis tareas</h1>
+        <Tarea texto="Aprender React" />
+      </div>
     </div>
   );
 }
